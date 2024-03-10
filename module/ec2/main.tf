2bc068c48b88d8afdc0b74ef5f41256eb36e5d27
Ashwini-Sagare <104609316+Ashwini-Sagare@users.noreply.github.com>
2024-03-10 22:41:43 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key_id
+  secret_key = var.secret_access_key
+}
 
+#ec2 creation
+resource "aws_instance" "windows_instances" {
+  count = var.count_value
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  tags = {
+    Name = "windowsec2-${count.index + 1}"
+  }
+}
+output "instance_ids" {
+  value = aws_instance.windows_instances[*].id
+}
