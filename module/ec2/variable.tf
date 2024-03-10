a1e813a91bc2c99de6e60ff4f142d325334c9c6f
Ashwini-Sagare <104609316+Ashwini-Sagare@users.noreply.github.com>
2024-03-10 22:46:28 +0530

    Add files via upload

---
+++ module/ec2/variable.tf
@@ -0,0 +1,20 @@
+variable "access_key_id" {
+  default = "AKIAXS22627VTLCRR3HP"
+}
+
+variable "secret_access_key" {
+  default = "kkIqs3uVkDU8uPiV3q2vhYbUK6DTzriW0CFJzl2n"
+}
+
+variable "ami_value" {
+  description = "ami value of windows instance"
+}
+
+variable "instance_type_value" {
+  description = "this is instance type"
+}
+
+variable "count_value" {
+  description = "Number of ec2 instance creation"
+}
+
